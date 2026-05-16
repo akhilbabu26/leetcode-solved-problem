d41fd599541d23f84661ee48973797db2de011bd
Akhil Babu <akhilbabu.go@gmail.com>
2026-05-16 09:52:30 +0530

    Time: 35 ms (65.57%) | Memory: 9.3 MB (72.95%) - LeetSync

--- 575-distribute-candies/distribute-candies.go
+++ 575-distribute-candies/distribute-candies.go
@@ -1,9 +1,9 @@
 func distributeCandies(candyType []int) int {
     toEat := len(candyType)/2
-    candy := make(map[int]int)
+    candy := make(map[int]struct{})
 
     for _, v := range candyType{
-        candy[v]++
+        candy[v] = struct{}{}
     }
 
     if len(candy) >= toEat{
