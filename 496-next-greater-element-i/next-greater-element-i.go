1323a7008c0036efda001ffff7696fd18293f301
Akhil Babu <akhilbabu.go@gmail.com>
2026-05-17 11:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 5.2 MB (37.93%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.go
@@ -0,0 +1,33 @@
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+    stack := []int{}
+	nextGreater := make(map[int]int)
+
+	for _, num := range nums2 {
+
+		for len(stack) > 0 && num > stack[len(stack)-1] {
+
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			nextGreater[top] = num
+		}
+
+		stack = append(stack, num)
+	}
+
+	for len(stack) > 0 {
+
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		nextGreater[top] = -1
+	}
+
+	result := []int{}
+
+	for _, num := range nums1 {
+		result = append(result, nextGreater[num])
+	}
+
+	return result
+}
