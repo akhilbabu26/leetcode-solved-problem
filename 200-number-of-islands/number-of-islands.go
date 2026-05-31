acd0085e922b679aba74dfa8d60842aad74a33f7
Akhil Babu <akhilbabu.go@gmail.com>
2026-05-31 10:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 5.6 MB (83.47%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.go
@@ -0,0 +1,38 @@
+func numIslands(grid [][]byte) int {
+    if len(grid) == 0{
+        return 0
+    }
+
+    rows := len(grid)
+    cols := len(grid[0])
+    count := 0
+
+    var dfs func(int, int)
+
+    dfs = func(r, c int){
+        if r < 0 || r >= rows || c < 0 || c >= cols{
+            return
+        }
+
+        if grid[r][c] == '0'{
+            return
+        }
+
+        grid[r][c] = '0'
+
+		dfs(r+1, c) 
+		dfs(r-1, c) 
+		dfs(r, c+1) 
+		dfs(r, c-1) 
+	}
+
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			if grid[r][c] == '1' {
+				count++
+				dfs(r, c)
+			}
+		}
+	}
+	    return count
+}
