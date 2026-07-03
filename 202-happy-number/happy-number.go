d6eeaf9b52ee2c58299530ed2e3a9119cf017ded
Akhil Babu <akhilbabu.go@gmail.com>
2026-07-03 19:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (12.56%) - LeetSync

---
+++ 202-happy-number/happy-number.go
@@ -0,0 +1,22 @@
+func isHappy(n int) bool {
+    visited := make(map[int]bool)
+
+    for n != 1 && !visited[n] {
+        visited[n] = true
+        n = sumOfSquares(n)
+    }
+
+    return n == 1
+}
+
+func sumOfSquares(n int) int {
+    sum := 0
+
+    for n > 0 {
+        digit := n % 10
+        sum += digit * digit
+        n /= 10
+    }
+
+    return sum
+}
